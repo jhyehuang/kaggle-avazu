d808d0124b766f1ed55d7c9b4b7e645e2a5a65dc
zhijie.huang <zhijie.huang@yulore.com>
2018-07-04 15:17:50 +0800

    commit

--- avazu_code/ml/gbdt.py
+++ avazu_code/ml/gbdt.py
@@ -149,7 +149,7 @@ kfold = StratifiedKFold(n_splits=5, shuffle=True, random_state=3)
 def done(istrain=True):
 #    test_save.drop('click',axis=1,inplace=True)
 #    op=['n_estimators','max_depth','subsample','reg_alpha']
-    op=['max_depth','subsample','reg_alpha']
+    op=['subsample','reg_alpha']
     if istrain:
         train_save = gdbt_data_get_train(1537)
         print(train_save.shape)
@@ -160,7 +160,7 @@ def done(istrain=True):
 #        n_estimators = [i for i in range(200,1000,1)]
         xgb1 = XGBClassifier(learning_rate =0.1,
         n_estimators=666,
-        max_depth=7,
+        max_depth=6,
         min_child_weight=1,
         gamma=0,
         subsample=0.8,
