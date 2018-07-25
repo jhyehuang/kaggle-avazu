d756493f54da72ff848e1ffccccc5c24a47dadd0
zhijie.huang <zhijie.huang@yulore.com>
2018-07-25 16:17:55 +0800

    commit

--- avazu_code/ml/ml_utils.py
+++ avazu_code/ml/ml_utils.py
@@ -505,7 +505,8 @@ def calc_exptv(data1,data2,data3,vn_list,add_count=False):
                 
                 day_exps[day_v][vn] = calcTVTransform(t3, vn, 'click', cred_k, filter_t1)
             new_list.append(vn)
-    t3.ix[t3.values ==31,[x for x in new_list if 'click' in x]]=0
+    click_col=[x for x in new_list if 'click' in x]
+    t3.ix[np.logical_and(t3.one_day.values ==31,True),click_col]=0
     t3.drop(['one_day'], axis=1,inplace = True)
     t3.drop(['click'], axis=1,inplace = True)
     t3.to_csv(FLAGS.tmp_data_path+'two_col_join.csv',index=False)
